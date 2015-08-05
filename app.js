27e383be874bc81f06e34efd3ff5b2178bb5e6ff
Ruslan Penkovskij <ruslan.penkovskij@gmail.com>
2015-08-05 16:53:07 +0300

    new interface for 4 exercise

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: "pages/home.html",
-            //controller: "mainController"
+            templateUrl: "pages/home.html"
         })
         .when('/1', {
             templateUrl: "pages/1.html",
@@ -37,23 +36,49 @@ app.config(function($routeProvider) {
 app.controller("mainController", function($scope, $timeout) {
     $scope.counter = 10;
     $scope.numer = true;
-    $scope.mytimeout = null;
     $scope.cpod = 1;
     $scope.total = 0;
-    $scope.sek = function() {
+    $scope.btn = true;
+    $scope.sek = function () {
         if ($scope.counter != 0) {
             $scope.numer = true;
             $scope.message = null;
             $scope.counter--;
-            $scope.total = Math.floor(($scope.cpod++)/10);
             $timeout($scope.sek, 1000);
             document.getElementById("myBtn").disabled = true;
-        }
-        else {
+        } else {
+            $scope.total++;
             $scope.numer = false;
             $scope.message = "Отдохни";
             $scope.counter = 10;
             document.getElementById("myBtn").disabled = false;
         }
     };
+    $scope.less = function() {
+        if($scope.total != 10) {
+            $scope.btn = false;
+            if ($scope.counter != 0) {
+                $scope.numer = true;
+                $scope.message = null;
+                $timeout($scope.less, 1000);
+                $scope.counter--;
+                document.getElementById("myBtn").disabled = true;
+            } else {
+                $scope.total++;
+                $scope.numer = false;
+                $scope.message = "Отдохни";
+                $scope.counter = 11;
+                document.getElementById("myBtn").disabled = false;
+                $timeout($scope.less, 10000);
+                if($scope.total != 10)
+                    $timeout($scope.prepare, 7000);
+            }
+        } else {
+            $scope.numer = false;
+            $scope.message = "Готово! Можете перейти к следующему упражнению.";
+        }
+    };
+    $scope.prepare = function() {
+        $scope.message = "Приготовься";
+    }
 });
